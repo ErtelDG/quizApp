db2d2b6b7ad2b75719f679a84ca5832064e0920b
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-09-24 19:07:59 +0200

    f

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
 let questions = [
-          {
-                    question: "Wer ist der Erfinder von Linux?",
-                    answer_1: "Linus Torvalds",
-                    answer_2: "Bill Gates",
-                    answer_3: "Steve Jobs",
-                    answer_4: "Mark Zuckerberg",
-                    answer_5: "Google(Firma)",
-                    right_answer: 1,
-          },
-          {
-                    question: "Wie viele Linux Distros gibt es ungefähr?",
-                    answer_1: "ca. 60",
-                    answer_2: "ca. 100",
-                    answer_3: "ca. 70",
-                    answer_4: "ca. 10",
-                    answer_5: "ca. 50",
-                    right_answer: 5,
-          },
+   {
+      question: "Wer ist der Erfinder von Linux?",
+      answer_1: "Linus Torvalds",
+      answer_2: "Bill Gates",
+      answer_3: "Steve Jobs",
+      answer_4: "Mark Zuckerberg",
+      answer_5: "Google(Firma)",
+      right_answer: 1,
+   },
+   {
+      question: "Wie viele Linux Distros gibt es ungefähr?",
+      answer_1: "ca. 60",
+      answer_2: "ca. 100",
+      answer_3: "ca. 70",
+      answer_4: "ca. 10",
+      answer_5: "ca. 50",
+      right_answer: 5,
+   },
 
-          {
-                    question: "Was davon ist keine Linux Oberfläche?",
-                    answer_1: "Plasma",
-                    answer_2: "Mate",
-                    answer_3: "Xfce",
-                    answer_4: "Elementary",
-                    answer_5: "Unity",
-                    right_answer: 4,
-          },
-          {
-                    question: "In welchem Bereich wird Linux am häufigsten eingesetzt?",
-                    answer_1: "Gaming",
-                    answer_2: "Server",
-                    answer_3: "Developing",
-                    answer_4: "Office",
-                    answer_5: "Desktop PCs",
-                    right_answer: 2,
-          },
-          {
-                    question: "Was davon ist keine Linux Distro?",
-                    answer_1: "Damn Small Linux",
-                    answer_2: "KDE",
-                    answer_3: "Peppermint",
-                    answer_4: "Manjaro",
-                    answer_5: "Poppy",
-                    right_answer: 2,
-          },
+   {
+      question: "Was davon ist keine Linux Oberfläche?",
+      answer_1: "Plasma",
+      answer_2: "Mate",
+      answer_3: "Xfce",
+      answer_4: "Elementary",
+      answer_5: "Unity",
+      right_answer: 4,
+   },
+   {
+      question: "In welchem Bereich wird Linux am häufigsten eingesetzt?",
+      answer_1: "Gaming",
+      answer_2: "Server",
+      answer_3: "Developing",
+      answer_4: "Office",
+      answer_5: "Desktop PCs",
+      right_answer: 2,
+   },
+   {
+      question: "Was davon ist keine Linux Distro?",
+      answer_1: "Damn Small Linux",
+      answer_2: "KDE",
+      answer_3: "Peppermint",
+      answer_4: "Manjaro",
+      answer_5: "Poppy",
+      right_answer: 2,
+   },
 
-          {
-                    question: "Was ist die ressourcensparendste Linux Oberfläche?",
-                    answer_1: "Mate",
-                    answer_2: "XFCE",
-                    answer_3: "Cinnamon",
-                    answer_4: "LXDE",
-                    answer_5: "KDE",
-                    right_answer: 4,
-          },
-          {
-                    question: "Was hiervon ist keine Paketverwaltung?",
-                    answer_1: "apt",
-                    answer_2: "packager",
-                    answer_3: "pacman",
-                    answer_4: "flatpack",
-                    answer_5: "slackware",
-                    right_answer: 2,
-          },
+   {
+      question: "Was ist die ressourcensparendste Linux Oberfläche?",
+      answer_1: "Mate",
+      answer_2: "XFCE",
+      answer_3: "Cinnamon",
+      answer_4: "LXDE",
+      answer_5: "KDE",
+      right_answer: 4,
+   },
+   {
+      question: "Was hiervon ist keine Paketverwaltung?",
+      answer_1: "apt",
+      answer_2: "packager",
+      answer_3: "pacman",
+      answer_4: "flatpack",
+      answer_5: "slackware",
+      right_answer: 2,
+   },
 
-          {
-                    question: "Was ist kein Desktop?",
-                    answer_1: "Plasma",
-                    answer_2: "Mate",
-                    answer_3: "Xfce",
-                    answer_4: "Elementary",
-                    answer_5: "Unity",
-                    right_answer: 4,
-          },
+   {
+      question: "Was ist kein Desktop?",
+      answer_1: "Plasma",
+      answer_2: "Mate",
+      answer_3: "Xfce",
+      answer_4: "Elementary",
+      answer_5: "Unity",
+      right_answer: 4,
+   },
 ];
 
 let score = 0;
@@ -84,140 +84,131 @@ let audioWrong = new Audio("audio/wrong.mp3");
 
 //start quiz
 function startQuiz() {
-          document.getElementById("startSide").style = "display:none";
-          document.getElementById("quizSideStart").style = " ";
-          init();
+   document.getElementById("startSide").style = "display:none";
+   document.getElementById("quizSideStart").style = " ";
+   init();
 }
 
 //render main start side
 function init() {
-          document.getElementById(
-                    "maxQuestions"
-          ).innerHTML = `${questions.length}`;
-          showQuestion();
+   document.getElementById("maxQuestions").innerHTML = `${questions.length}`;
+   showQuestion();
 }
 
 //show question
 function showQuestion() {
-          if (currentQuestion >= questions.length) {
-                    showEndResult();
-          } else {
-                    showNextQuestion();
-          }
+   if (currentQuestion >= questions.length) {
+      showEndResult();
+   } else {
+      showNextQuestion();
+   }
 }
 
 //check answer and go next question
 function answer(idValue) {
-          let questionRightAnswer = questions[currentQuestion].right_answer;
-          let questionLastValue = idValue.slice(-1);
+   let questionRightAnswer = questions[currentQuestion].right_answer;
+   let questionLastValue = idValue.slice(-1);
 
-          if (questionLastValue == questionRightAnswer) {
-                    rightAnswer(idValue);
-          } else {
-                    wrongAnswer(idValue, questionRightAnswer);
-          }
-          disableAnswer();
+   if (questionLastValue == questionRightAnswer) {
+      rightAnswer(idValue);
+   } else {
+      wrongAnswer(idValue, questionRightAnswer);
+   }
+   disableAnswer();
 
-          document.getElementById("sendButton").disabled = false;
+   document.getElementById("sendButton").disabled = false;
 }
 
 function showEndResult() {
-          progressBar();
-          document.getElementById("endScreen").style = "";
-          document.getElementById("questionBody").style = "display:none;";
-          document.getElementById("imgQuiz").src = "img/cup.jpg";
+   progressBar();
+   document.getElementById("endScreen").style = "";
+   document.getElementById("questionBody").style = "display:none;";
+   document.getElementById("imgQuiz").src = "img/cup.jpg";
 
-          document.getElementById("maxQuestionsEnd").innerHTML =
-                    questions.length;
-          document.getElementById("resultPlayer").innerHTML = score;
+   document.getElementById("maxQuestionsEnd").innerHTML = questions.length;
+   document.getElementById("resultPlayer").innerHTML = score;
 }
 
 function showNextQuestion() {
-          let question = questions[currentQuestion];
-          document.getElementById("questiontext").innerHTML = question.question;
-          document.getElementById("answer_1").innerHTML = question.answer_1;
-          document.getElementById("answer_2").innerHTML = question.answer_2;
-          document.getElementById("answer_3").innerHTML = question.answer_3;
-          document.getElementById("answer_4").innerHTML = question.answer_4;
-          document.getElementById("answer_5").innerHTML = question.answer_5;
-          enableAnswer();
-          progressBar();
+   let question = questions[currentQuestion];
+   document.getElementById("questiontext").innerHTML = question.question;
+   document.getElementById("answer_1").innerHTML = question.answer_1;
+   document.getElementById("answer_2").innerHTML = question.answer_2;
+   document.getElementById("answer_3").innerHTML = question.answer_3;
+   document.getElementById("answer_4").innerHTML = question.answer_4;
+   document.getElementById("answer_5").innerHTML = question.answer_5;
+   enableAnswer();
+   progressBar();
 }
 
 function rightAnswer(idValue) {
-          document.getElementById(idValue).classList.add("bg-success");
-          audioSuccess.play();
-          score++;
+   document.getElementById(idValue).classList.add("bg-success");
+   audioSuccess.play();
+   score++;
 }
 
 function wrongAnswer(idValue, questionRightAnswer) {
-          document.getElementById(idValue).classList.add("bg-danger");
-          let rightID = "answer_" + questionRightAnswer;
-          document.getElementById(`${rightID}`).classList.add("bg-success");
-          audioWrong.play();
+   document.getElementById(idValue).classList.add("bg-danger");
+   let rightID = "answer_" + questionRightAnswer;
+   document.getElementById(`${rightID}`).classList.add("bg-success");
+   audioWrong.play();
 }
 
 function disableAnswer() {
-          document.getElementById("answerBox1").style.pointerEvents = "none";
-          document.getElementById("answerBox2").style.pointerEvents = "none";
-          document.getElementById("answerBox3").style.pointerEvents = "none";
-          document.getElementById("answerBox4").style.pointerEvents = "none";
-          document.getElementById("answerBox5").style.pointerEvents = "none";
+   document.getElementById("answerBox1").style.pointerEvents = "none";
+   document.getElementById("answerBox2").style.pointerEvents = "none";
+   document.getElementById("answerBox3").style.pointerEvents = "none";
+   document.getElementById("answerBox4").style.pointerEvents = "none";
+   document.getElementById("answerBox5").style.pointerEvents = "none";
 }
 
 function enableAnswer() {
-          document.getElementById("answerBox1").style.pointerEvents = "auto";
-          document.getElementById("answerBox2").style.pointerEvents = "auto";
-          document.getElementById("answerBox3").style.pointerEvents = "auto";
-          document.getElementById("answerBox4").style.pointerEvents = "auto";
-          document.getElementById("answerBox5").style.pointerEvents = "auto";
+   document.getElementById("answerBox1").style.pointerEvents = "auto";
+   document.getElementById("answerBox2").style.pointerEvents = "auto";
+   document.getElementById("answerBox3").style.pointerEvents = "auto";
+   document.getElementById("answerBox4").style.pointerEvents = "auto";
+   document.getElementById("answerBox5").style.pointerEvents = "auto";
 }
 
 function nextQuestion() {
-          currentQuestion++; //increases counter for next element in json
+   currentQuestion++; //increases counter for next element in json
 
-          document.getElementById("sendButton").disabled = true; //disable button
-          //show current question
-          document.getElementById("currentQuestion").innerHTML = `${
-                    currentQuestion + 1
-          }`;
-          resetAnswerButtons(); //reset background color answer
-          showQuestion(); //show next question
+   document.getElementById("sendButton").disabled = true; //disable button
+   //show current question
+   document.getElementById("currentQuestion").innerHTML = `${
+      currentQuestion + 1
+   }`;
+   resetAnswerButtons(); //reset background color answer
+   showQuestion(); //show next question
 }
 
 //reset background color answer
 function resetAnswerButtons() {
-          for (let index = 1; index < 6; index++) {
-                    const element = "answer_" + index;
-                    document.getElementById(element).classList.remove(
-                              "bg-success"
-                    );
-                    document.getElementById(element).classList.remove(
-                              "bg-danger"
-                    );
-          }
+   for (let index = 1; index < 6; index++) {
+      const element = "answer_" + index;
+      document.getElementById(element).classList.remove("bg-success");
+      document.getElementById(element).classList.remove("bg-danger");
+   }
 }
 
 //show progress bar questions
 function progressBar() {
-          let progress = currentQuestion;
-          let allQestions = questions.length;
-          let progressInPercent = Math.round((100 / allQestions) * progress);
+   let progress = currentQuestion;
+   let allQestions = questions.length;
+   let progressInPercent = Math.round((100 / allQestions) * progress);
 
-          document.getElementById(
-                    `progressBar`
-          ).style = `width: ${progressInPercent}%`;
-          document.getElementById(
-                    `progressBar`
-          ).innerHTML = `${progressInPercent}%`;
+   document.getElementById(
+      `progressBar`
+   ).style = `width: ${progressInPercent}%`;
+   document.getElementById(`progressBar`).innerHTML = `${progressInPercent}%`;
 }
 
 function restartGame() {
-          document.getElementById("imgQuiz").src = "img/question.jpg";
-          document.getElementById("endScreen").style = "display:none;";
-          document.getElementById("questionBody").style = "";
-          document.getElementById("currentQuestion").innerHTML = 1;
-          score = 0;
-          currentQuestion = 0;
-          init();
+   document.getElementById("imgQuiz").src = "img/question.jpg";
+   document.getElementById("endScreen").style = "display:none;";
+   document.getElementById("questionBody").style = "";
+   document.getElementById("currentQuestion").innerHTML = 1;
+   score = 0;
+   currentQuestion = 0;
+   init();
 }
